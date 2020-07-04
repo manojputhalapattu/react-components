87e664f74a9fe7f6e8beaf887ac8ed329189b0e2
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-04 23:42:02 +0530

    "Custom-Component"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-function Heading(){
-  return <h1>this is it </h1>
-}
+import Heading from "./Heading";
+import List from "./list";
+
 ReactDOM.render(
   <div>
-    <Heading/>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
