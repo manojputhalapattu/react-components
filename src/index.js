83ec359fd7c58d3b2d4520f641b5285110efab03
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-04 23:19:40 +0530

    "Heading-function"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+function Heading(){
+  return <h1>this is it </h1>
+}
 ReactDOM.render(
   <div>
+    <Heading/>
     <h1>My Favourite Foods</h1>
     <ul>
       <li>Bacon</li>
